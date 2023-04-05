918b4c15f98216b998678fb026dd32a8fd4862a8
rojinpra7 <89182903+rojinpra7@users.noreply.github.com>
2023-04-05 08:59:57 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rojin Prajapati on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
